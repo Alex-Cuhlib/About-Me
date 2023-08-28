d89667290871ec49c63ce329a6eac396fa319468
Александр <Alex.Cuhlib@gmail.com>
2023-08-28 17:40:44 +0300

    Add files via upload

---
+++ About Me.py
@@ -0,0 +1,13 @@
+#int
+number = 5
+age = 29
+
+#float
+fnumber = 5.7
+
+#str
+name = "Александр"
+
+#Конкатенация
+print  ( "Привет, меня зовут " + name + "!" )
+print ( "Мне " + str(age) + " лет!" )
